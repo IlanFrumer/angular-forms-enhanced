bc25e196fa1be4c87204621235048cad6b6e4582
ilanfrumer <ilanfrumer@gmail.com>
2014-02-07 16:57:34 +0200

    first

---
+++ angular-forms-enhanced.js
@@ -0,0 +1,199 @@
+/*global angular:false */
+
+(function (angular) {
+
+  'use strict';
+
+  var app = angular.module('ngFormsEnhanced',[]);
+
+  var PRISTINE_CLASS = 'ng-pristine',
+      DIRTY_CLASS = 'ng-dirty';
+
+  var noop = angular.noop;
+
+  var nullFormCtrl = {
+    $addControl: noop,
+    $removeControl: noop,
+    $setValidity: noop,
+    $setDirty: noop,
+    $setPristine: noop,
+    $updatePristine: noop
+  };
+
+  var formDirective = function(directiveName, restrict) {
+    app.directive(directiveName, function() {
+      return {
+        restrict: restrict,
+        require: 'form',
+        compile: function() {
+          return {
+            pre: function(scope, element, attrs, form) {
+              var parentForm = element.parent().controller('form') || nullFormCtrl;
+              
+              var $addControl = form.$addControl,
+                  $removeControl = form.$removeControl;
+              
+              form.$controls = [];
+              
+              form.$addControl = function(control) {
+                form.$controls.push(control);
+                $addControl(control);
+              };
+
+              form.$removeControl = function(control) {
+                var index = form.$controls.indexOf(control);
+                if (index >= 0) {
+                  form.$controls.splice(index, 1);
+                }
+                $removeControl(control);
+              };
+
+              // new method
+              form.$revert = function () {
+                for (var i = 0; i < form.$controls.length; i+=1) {
+                  if (form.$controls[i].$dirty) {
+                    form.$controls[i].$revert();
+                  }
+                }
+                form.$setPristine();
+                form.$updatePristine();
+              };
+                
+              // new method
+
+              form.$updatePristine = function() {
+                for (var i = 0; i < form.$controls.length; i+=1) {
+                  if (form.$controls[i].$dirty) {
+                    return;
+                  }
+                }
+       
+                form.$setPristine();
+       
+                if (parentForm) {
+                  parentForm.$updatePristine();
+                }
+              };
+            }
+          };
+        }
+      };
+    });
+  };
+   
+  formDirective('form', 'E');
+  formDirective('ngForm', 'EAC');
+   
+  app.directive('ngModel', ['$parse','$exceptionHandler', function($parse, $exceptionHandler) {
+
+    return {
+      require: ['ngModel','^?form'],
+      compile: function(tElement, tAttrs) {
+
+        var ngModelGet = $parse(tAttrs.ngModel),
+            ngModelSet = ngModelGet.assign;
+
+        return {
+          pre: function(scope, element, attrs, ctrl) {
+
+            var ngModel = ctrl[0],
+                parentForm = ctrl[1] || nullFormCtrl;
+
+            var $original = (function () {
+              var $value = null,
+                  $isEmpty = true;
+              return {
+                get: function() {
+                  return $value;
+                },
+                set: function(val) {
+                  if (ngModel.$pristine) {
+                    $value = ngModel.$modelValue;
+                    $isEmpty = ngModel.$isEmpty($value);
+                  }
+                  // let value pass in $formatters
+                  return val;
+                },
+                equals: function(val) {
+                  if(ngModel.$invalid) {
+                    return false;
+                  } else if($isEmpty) {
+                    return ngModel.$isEmpty(val);
+                  } else {
+                    return angular.equals($value,val);
+                  }
+                }
+              };
+            }());
+
+            ngModel.$formatters.unshift($original.set);
+
+            // new method
+
+            ngModel.$updatePristine = function() {
+              parentForm.$updatePristine();
+            };
+
+            // new method
+
+            ngModel.$revert = function() {
+              ngModelSet(scope, $original.get());
+            };
+
+            // wrap
+            var $setPristine = ngModel.$setPristine;
+            
+            ngModel.$setPristine = function() {
+              $setPristine.call(ngModel);
+              $original.set();
+            };
+
+            // monkey patching $setViewValue
+
+            ngModel. $setViewValue = function(value) {
+
+              ngModel.$viewValue = value;
+
+              angular.forEach(ngModel.$parsers, function(fn) {
+                value = fn(value);
+              });
+
+              if (ngModel.$modelValue !== value) {
+
+                ngModel.$modelValue = value;
+
+                if (ngModel.$dirty) {
+                  // rollback to pristine
+                  if ($original.equals(value)) {
+                    ngModel.$setPristine();
+                    ngModel.$updatePristine();
+                  }
+                } else {
+                  if(!$original.equals(value)) {
+                    // change to dirty
+                    ngModel.$dirty = true;
+                    ngModel.$pristine = false;
+                    element.removeClass(PRISTINE_CLASS).addClass(DIRTY_CLASS);
+                    parentForm.$setDirty();
+                  }
+                }
+
+                ngModelSet(scope, value);
+                angular.forEach(ngModel.$viewChangeListeners, function(listener) {
+                  try {
+                    listener();
+                  } catch(e) {
+                    $exceptionHandler(e);
+                  }
+                });
+              }
+            };
+          }
+        };
+      }
+    };
+  }]);
+
+}(angular));
+
+
---
+++ app.js
@@ -0,0 +1,48 @@
+var app = angular.module('demo', ['ngFormsEnhanced']);
+
+app.controller('MainCtrl', function($scope) {
+  $scope.list = [
+    
+    { key : "help" , en : "Help" , he: "עזרה"},
+    { key : "want" , en : "Want" , he: "רוצה"},
+    { key : "email" , en : "Email" }
+  ]
+
+  $scope.user = "Ilan Frumer";
+  $scope.person = "Misko";
+  
+  $scope.save = function(item, form){
+    form.$setPristine();
+    form.$updatePristine();
+    console.log("saved all");
+  }
+
+  $scope.saveAll = function(form){
+    console.log("saved all");
+    form.$setPristine();
+  }
+});
+
+
+app.directive("contenteditable", function() {
+  return {
+    require: "?ngModel",
+    link: function(scope, element, attrs, ngModel) {
+      if (ngModel) {
+
+        // DOM -> view
+        element.bind("blur", function() {
+          var val = element.html();
+          scope.$apply(function(){
+            ngModel.$setViewValue(val);
+          });
+        });
+        
+        // view -> DOM
+        ngModel.$render = function() {
+          element.html(ngModel.$viewValue);
+        };
+      }
+    }
+  };
+});
